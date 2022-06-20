737d3387de390f6c05dd1d6bab063fd2e82a07d9
Manuel <manueulosada@gmail.com>
2022-06-20 13:07:50 -0300

    Primer ejercicio

---
+++ Ejercicios/01_variables_and_operators.js
@@ -0,0 +1,26 @@
+// 1. Variables y Operadores
+console.log("Ejercicios 1")
+
+// Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+let a = "Hola";
+let b = "mundo"
+let c = a.concat(" ", b);
+
+console.log("")
+console.log("Ejercicio 1.1")
+console.log(c);
+
+
+// Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+
+
+let str1 = "Hola";
+let str2 = "mundo"
+let sum = str1.length + str2.length;
+
+console.log("")
+console.log("Ejercicio 1.2")
+console.log(sum);
+console.log("")
+console.log("-------------------------------------------------------------")
